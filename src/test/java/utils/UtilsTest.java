91b406d728372d561b92c3453d92b17323911794
Ferrorist <ferrorist@naver.com>
2024-11-06 00:15:07 +0900

    chore(Utils): 패키지 및 클래스명 변경 (Config → Utils)
    Config보다는 Utils에 맞는 기능들이므로, 이에 맞게 수정했습니다.

--- src/test/java/utils/UtilsTest.java
+++ src/test/java/utils/UtilsTest.java
@@ -1,22 +1,22 @@
-package config;
+package utils;
 
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ConfigTest {
+public class UtilsTest {
     private final String productionPromotionFilePath = "src/main/resources/promotions.md";
 
     @Test
     void readMarkDownFileTest() {
         Assertions.assertDoesNotThrow(() -> {
-            MarkDownConfig.readMarkDownFile(productionPromotionFilePath);
+            MarkDownUtils.readMarkDownFile(productionPromotionFilePath);
         });
     }
 
     @Test
     void StringSplitTest() {
-        Assertions.assertEquals(List.of("name", "buy", "get"), Arrays.asList(StringConfig.splitLine("  name, buy  ,get     ")));
+        Assertions.assertEquals(List.of("name", "buy", "get"), Arrays.asList(StringUtils.splitLine("  name, buy  ,get     ")));
     }
 }
