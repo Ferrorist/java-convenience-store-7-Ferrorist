2a9f9bbe2728502ead6604cffc62996d448b368e
Ferrorist <ferrorist@naver.com>
2024-11-05 23:21:53 +0900

    feat(Promotion): 프로모션 객체 생성
    프로모션 객체 클래스를 생성했습니다.

---
+++ src/main/java/store/models/Promotion.java
@@ -0,0 +1,18 @@
+package store.models;
+
+public class Promotion {
+    private String name;
+    private int buyQuantity;
+    private int freeQuantity;
+
+    private String startDate;
+    private String endDate;
+
+    public Promotion(String name, int buyQuantity, int freeQuantity, String startDate, String endDate) {
+        this.name = name;
+        this.buyQuantity = buyQuantity;
+        this.freeQuantity = freeQuantity;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
