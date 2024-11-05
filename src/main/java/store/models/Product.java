b78666c6bf921adb51a591194c51c6b21f4886c7
Ferrorist <ferrorist@naver.com>
2024-11-05 23:21:12 +0900

    feat(Product): 상품 객체 생성
    상품 객체 클래스를 생성했습니다.

---
+++ src/main/java/store/models/Product.java
@@ -0,0 +1,20 @@
+package store.models;
+
+public class Product {
+    private final String name;
+    private final int price;
+    private int quantity;
+    private final Promotion productPromotion;
+
+
+    public Product(String name, int price, int quantity, Promotion productPromotion) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.productPromotion = productPromotion;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
