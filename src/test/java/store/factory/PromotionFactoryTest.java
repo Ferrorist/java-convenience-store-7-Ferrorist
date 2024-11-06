265196ac77bead9769580fd35c3fa9c7f56715c3
parkdamin <ferrorist@naver.com>
2024-11-06 17:24:59 +0900

    fix(GeneratePromotionTest): 프로모션 객체 생성 테스트 코드 수정
    날짜 문자열을 체크하도록 수정한 로직을 반영했습니다.

--- src/test/java/store/factory/PromotionFactoryTest.java
+++ src/test/java/store/factory/PromotionFactoryTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import store.model.Promotion;
 
-public class FactoryTest {
+public class PromotionFactoryTest {
     @Test
     void GeneratePromotionTest() {
         Assertions.assertEquals(
-                new Promotion("A", 1, 2, "2000-1-1", "2000-02-01"),
-                PromotionFactory.createPromotion("A, 1, 2, 2000-1-1, 2000-02-01")
+                new Promotion("A", 1, 2, "2000-01-01", "2000-02-01"),
+                PromotionFactory.createPromotion("A, 1, 2, 2000-01-01, 2000-02-01")
                 );
     }
 
