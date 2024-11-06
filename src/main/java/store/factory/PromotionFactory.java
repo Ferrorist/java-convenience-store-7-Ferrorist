49c9ee01fabbc5ec82f618dc9a24722a48dbc187
parkdamin <ferrorist@naver.com>
2024-11-06 17:19:30 +0900

    fix(move checkAndparseInt): 메서드 이동
    checkAndparseInt 메서드를 PromotionFactory에서 StringUtils으로 이동시켰습니다.

--- src/main/java/store/factory/PromotionFactory.java
+++ src/main/java/store/factory/PromotionFactory.java
@@ -10,8 +10,8 @@ public class PromotionFactory {
 
     public static Promotion createPromotion(String[] inputs) throws IllegalArgumentException {
         if (checkInputArgument(inputs)) {
-            int buyQuantity = checkAndparseInt(inputs[1]);
-            int freeQuantity = checkAndparseInt(inputs[2]);
+            int buyQuantity = StringUtils.checkAndparseInt(inputs[1]);
+            int freeQuantity = StringUtils.checkAndparseInt(inputs[2]);
 
             return new Promotion(inputs[0], buyQuantity, freeQuantity, inputs[3], inputs[4]);
         }
@@ -25,13 +25,5 @@ public class PromotionFactory {
         }
 
         return true;
-    }
-
-    private static int checkAndparseInt(String input) {
-        try {
-            return Integer.parseInt(input);
-        } catch (NumberFormatException exception) {
-            throw new NumberFormatException("[ERROR] 올바르지 않는 프로모션 입력값입니다.");
-        }
     }
 }
