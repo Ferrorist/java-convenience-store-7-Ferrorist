283d2d086d1677bd0370b9fea86a0998162ef60d
Ferrorist <ferrorist@naver.com>
2024-11-06 14:09:57 +0900

    feat(String Object strip): 문자열 split and strip 기능 구현
    주어진 문자열을 쉼표 기준으로 모두 split를 실행합니다.
    이후, 모든 문자열에 strip을 실행한 결과를 array나 List로 반환합니다.

---
+++ src/main/java/utils/StringUtils.java
@@ -0,0 +1,24 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StringUtils {
+
+    public static String[] splitLinetoArray(String line) {
+        String[] values = line.split(",");
+
+        for (int i = 0; i < values.length; i++) {
+            values[i] = values[i].strip();
+        }
+
+        return values;
+    }
+
+    public static List<String> splitLineToList(String line) {
+        return Stream.of(line.split(","))
+                .map(String::strip)
+                .toList();
+    }
+}
