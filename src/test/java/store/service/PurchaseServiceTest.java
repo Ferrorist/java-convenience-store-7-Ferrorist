e2461355e09e690f763270aa43312a32cce3a75a
parkdamin <ferrorist@naver.com>
2024-11-07 14:08:54 +0900

    test(checkProductStocks): 요청 물품 구매 여부 기능 테스트 코드 추가
    요청한 물품의 구매 여부를 판단하는 기능에 대한 테스트 코드를 작성했습니다.

--- src/test/java/store/service/PurchaseServiceTest.java
+++ src/test/java/store/service/PurchaseServiceTest.java
@@ -17,6 +17,17 @@ public class PurchaseServiceTest {
         Assertions.assertFalse(purchaseService.validatePurchaseRequest("{사이다-3}, [물2]"));
     }
 
+    @Test
+    void checkProductStocksTest() {
+        Assertions.assertTrue(purchaseService.checkProductStocks("[사이다-3]"));
+    }
+
+    @Test
+    void checkProductStocksExceptionTest() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+           purchaseService.checkProductStocks("[컵라면-100]");
+        });
+    }
 
     @BeforeAll
     static void beforeAll() {
